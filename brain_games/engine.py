9c134b3d092752d17db5b9f0d6880569cab0e0d4
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2025-09-25 23:39:50 +0300

    changes for hexlet

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,24 +2,19 @@ from brain_games.cli import welcome_user
 
 
 def run_game(game_logic, rounds=3):
+    print()
+    name = welcome_user() 
 
-    print("Welcome to the Brain Games!")
-    name = welcome_user()
-    correct_count = 0
-
-    while correct_count < rounds:
+    for _ in range(rounds):
         question, correct_answer = game_logic()
         print(f"Question: {question}")
-        answer = input("Your answer: ").strip()
-
+        answer = input("Your answer: ")
         if answer != str(correct_answer):
             print(
-                f"'{answer}' is wrong answer ;(. "
-                f"Correct answer was '{correct_answer}'."
+                f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."
             )
             print(f"Let's try again, {name}!")
             return
         print("Correct!")
-        correct_count += 1
 
     print(f"Congratulations, {name}!")
